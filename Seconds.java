2da2294ac88d9652f06123ea0fa589de4041a71e
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2023-09-09 14:42:14 +0530

    Add files via upload

--- Seconds.java
+++ Seconds.java
@@ -1,15 +1,29 @@
 public class Seconds {
 
     public static void main(String[] args){
-        takesTime(8324);
+        takesTime(3900);
+        takesTime(-3900);
     }
     public static void takesTime(int sec){
-        int hour = (sec/3600);
-        int rem=sec%3600;
-        int min=rem/60;
-        int seconds=rem%60;
-
-        System.out.println("the in Hours Min and Sec is "+ hour+" Hr : "+min+" : Min : "+seconds+" Sec" );
+        int hour ;
+        int rem;
+        int min;
+        int seconds;
+        if(sec >= 0){
+            hour = (sec/3600);
+             rem=sec%3600;
+             min=rem/60;
+             seconds=rem%60;
+            System.out.println("the in Hours Min and Sec is "+ hour+" Hr : "+min+" : Min : "+seconds+" Sec" );
+        }else {
+            System.out.println("Invlaid  Timeline");
+        }
 
     }
 }
+
+/*when you div 3900 ie seconds by 3600 you will get hour
+and when u take the remainder ie 300 (3900%3600) and divide
+that reaminder with 60 u will get minutes. then
+ when you take mod of the remainder you will get the remaining seconds
+ */
