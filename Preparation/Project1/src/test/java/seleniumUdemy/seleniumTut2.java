fd165aa141c2e66c257d5672c44cccad8e4bbbc9
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2024-01-15 14:30:57 +0530

    Update seleniumTut2.java

--- Preparation/Project1/src/test/java/seleniumUdemy/seleniumTut2.java
+++ Preparation/Project1/src/test/java/seleniumUdemy/seleniumTut2.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -23,8 +24,11 @@ public class seleniumTut2 {
   @Test(priority=1)
   public void login() {
 	  driver.get("https://rahulshettyacademy.com/loginpagePractise/");
-	  driver.findElement(By.id("username")).sendKeys("rahulshettyacademy");
-	  driver.findElement(By.id("password")).sendKeys("learning");
+	  //driver.findElement(By.id("username")).sendKeys("rahulshettyacademy");
+	  String username=getUsername(driver);
+	  String password=getPassowrd(driver);
+	  driver.findElement(By.id("username")).sendKeys(username);
+	  driver.findElement(By.id("password")).sendKeys(password);
 	  driver.findElement(By.xpath("//*[@id=\"login-form\"]/div[4]/div/label[1]/span[2]")).click();
 	  driver.findElement(By.xpath("//*[@id=\"login-form\"]/div[5]/select/option[1]")).click();
 	  driver.findElement(By.cssSelector("input[type='checkbox']")).click();
@@ -36,5 +40,22 @@ public class seleniumTut2 {
 	 Thread.sleep(3000);
 	 System.out.println(driver.findElement(By.className("navbar-brand")).getText());
 	 Assert.assertEquals(driver.findElement(By.className("navbar-brand")).getText(), "ProtoCommerce");
+ }
+ 
+ public String getUsername(WebDriver driver) {
+	 driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+	 WebElement user=driver.findElement(By.xpath("//*[@id=\"login-form\"]/div[7]/p/b[1]/i"));
+	 String userName=user.getText();
+	 return userName;
+ }
+ 
+ 
+ 
+ public  String getPassowrd(WebDriver driver) {
+	 driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+	 WebElement pass=driver.findElement(By.xpath("//*[@id=\"login-form\"]/div[7]/p/b[2]/i"));
+	 String password=pass.getText();
+	 return password;
+	 
  }
 }
